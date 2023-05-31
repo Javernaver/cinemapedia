77605c1ff8e2661f2e89e4e3098a0b7bb4409b12
Javier del Canto <javernaver@gmail.com>
2023-05-30 21:24:57 -0400

    router

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: false,
+        colorSchemeSeed: const Color(0xFF2862F5),
+      );
+}
