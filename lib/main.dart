dbb3d5bd4877d12931ca61e0086190e4ae402208
Javier del Canto <javernaver@gmail.com>
2023-06-02 20:07:55 -0400

    riverpod added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   if (kReleaseMode) {
@@ -11,7 +12,9 @@ Future<void> main() async {
   } else {
     await dotenv.load(fileName: '.env.development');
   }
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
